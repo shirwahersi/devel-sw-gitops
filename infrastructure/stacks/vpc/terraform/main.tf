567a5ae116671074916f1a09805de960880480a8
Shirwa Hersi <shirwa.hersi@theshopworks.com>
2022-11-03 21:05:09 +0000

    Initial commit

---
+++ infrastructure/stacks/vpc/terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.1.0"
+}
+
+variable "subject" {
+   type = string
+   default = "tfctl-rc"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "hey hey ya, ${var.subject}!"
+}
